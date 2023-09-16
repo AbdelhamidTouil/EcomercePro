987647668cc5ab83066086e6b56c30bc23617d6f
AbdelhamidDev <abdelhamidtouild@gmail.com>
2023-09-16 13:51:53 +0100

    seconde commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Product;
 
 class AdminController extends Controller
 {
@@ -28,29 +29,42 @@ class AdminController extends Controller
     }
 
     /**
-     * Display the resource.
-     */
-    public function show()
+     * view_product.
+     */ 
+    public function view_product()
     {
-        //
+       $category = Category::all();
+        return view('admin.product',compact('category'));
     }
+   
+    
 
-    /**
-     * Show the form for editing the resource.
-     */
-    public function edit()
+    public function add_product(Request $request)
     {
-        //
-    }
+       
+               $product = new product;
+               $product->title = $request->title;
+               $product->description = $request->description;
+               $product->price = $request->price;
+               $product->quantity = $request->quantity;
+               $product->discount_price = $request->discount_price;
+               $product->category=$request->category;
 
-    /**
-     * Update the resource in storage.
-     */
-    public function update(Request $request)
-    {
-        //
-    }
+               $image = $request->image;
+               $imagename = time() . '.' . $image->getClientOriginalExtension();
+               $request->image->move('product', $imagename);
+               $product->image=$imagename;
 
+
+
+
+        
+
+
+
+               $product->save();
+               return redirect()->back();
+    }
     /**
      * Remove the resource from storage.
      */
